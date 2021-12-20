98c34d701eea587b146a9047a3f535bc0ddaf2cb
Sohaib Furqan <sohaibfurqan@gmail.com>
2021-12-20 21:57:05 +0000

    Added new components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Navbar from './components/Navbar';
 import FeedbackList from './components/FeedbackList';
 import { useState } from 'react';
 import data from './data/FeedbackData';
+import FeedbackForm from './components/FeedbackForm';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
   const [feedback, setFeedback] = useState(data);
   return (
     <>
-      <Navbar bgColor='#131428' color='#ff6a95' />;
+      <Navbar bgColor='#131428' color='#ff6a95' />
       <div className='container'>
+        <FeedbackForm />
+        <FeedbackStats />
         <FeedbackList feedback={feedback} />
       </div>
     </>
