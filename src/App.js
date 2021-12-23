44da9e90d7076ea674022f92faa763f77c4a05df
Sohaib Furqan <sohaibfurqan@gmail.com>
2021-12-23 13:38:47 +0000

    Added routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackStats from './components/FeedbackStats';
 import { FeedbackProvider } from './context/FeedbackContext';
+import AboutIcon from './components/AboutIcon';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   return (
     <FeedbackProvider>
-      <Navbar />
-      <div className='container'>
-        <FeedbackForm />
-        <FeedbackStats />
-        <FeedbackList />
-      </div>
+      <Router>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+        <AboutIcon />
+      </Router>
     </FeedbackProvider>
   );
 }
